cffb31c3fc2ed59afd0133d7bf70d165bf1fee8d
kostaivanov <kostasvetlozarov@gmail.com>
2021-06-06 18:45:09 +0300

    Update BetweenFrames.cs, Prefabs.meta, and 4 more files...

--- labyrinth/Assets/BetweenFrames.cs
+++ labyrinth/Assets/BetweenFrames.cs
@@ -6,26 +6,33 @@ public class BetweenFrames : MonoBehaviour
 {
     [SerializeField] private FadeInOut fadeInOutScript;
     // Start is called before the first frame update
+    internal bool finishedFading = false;
     void Start()
     {
-        
+        fadeInOutScript.whiteFade.canvasRenderer.SetAlpha(0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(fadeInOutScript.whiteFade.canvasRenderer.GetAlpha());
+        if (fadeInOutScript.whiteFade.canvasRenderer.GetAlpha() == 1 && finishedFading == false)
+        {
+            fadeInOutScript.FadeOut();
+            finishedFading = true;
+        }
+
+        if (finishedFading == true && fadeInOutScript.whiteFade.canvasRenderer.GetAlpha() == 0)
+        {
+            finishedFading = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D otherObject)
     {
         if (otherObject.tag == "Player")
         {
-            fadeInOutScript.whiteFade.canvasRenderer.SetAlpha(0.0f);
-
-            Debug.Log("asdsad");
             fadeInOutScript.FadeIn();
-            //fadeInOutScript.FadeOut();
         }   
     }
 }
